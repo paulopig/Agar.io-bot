fa3f79859321c65d1923a599c4df30be3003afd3
paulopig <porkprg@live.com>
2015-08-22 11:07:52 -0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1321,7 +1321,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["W=TeamBr"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
